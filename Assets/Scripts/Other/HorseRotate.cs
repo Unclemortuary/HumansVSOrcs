d725068b36e6d58b04f42b61c217f40dcca487fa
Home\Алексей <myepam2017rts@gmail.com>
2017-08-21 12:32:06 +0300

    Merge branch 'От_20170815' of https://bitbucket.org/drumer_boy/humans-vs-orcs into От_20170815
    Здания для орков

---
+++ Assets/Scripts/Other/HorseRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorseRotate : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public Vector3 direction;
+
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (direction * speed * Time.deltaTime);
+	}
+}
