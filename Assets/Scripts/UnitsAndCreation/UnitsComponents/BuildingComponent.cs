40b054e76671ba666e69a8e2557c997ddf5942ae
Unclemortuary <artem_mihalych@mail.ru>
2017-08-23 15:42:55 +0300

    A few changes in BuildingComponent & BuildingConstructionHandler classes (in case to storage of color and shader of building)

--- Assets/Scripts/UnitsAndCreation/UnitsComponents/BuildingComponent.cs
+++ Assets/Scripts/UnitsAndCreation/UnitsComponents/BuildingComponent.cs
@@ -1,3 +1,16 @@
+using UnityEngine;
+
 public class BuildingComponent : RTSMonoBehaviour{
 
+	private Shader originShader;
+	private Color originColor;
+
+	public Shader OriginShader { get { return originShader; } set { originShader = value; } }
+	public Color OriginColor {get { return originColor; } set { originColor = value; } }
+
+	void Awake()
+	{
+		originShader = GetComponentsInChildren<Renderer> ()[0].material.shader;
+		originColor = GetComponentsInChildren<Renderer> ()[0].material.color;
+	}
 }
