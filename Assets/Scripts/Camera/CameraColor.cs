912900bc73678cbf003675109b965e442061df55
Home\Алексей <myepam2017rts@gmail.com>
2017-08-26 22:05:27 +0300

    Миникарта, мелкие изменения

--- Assets/Scripts/Camera/CameraColor.cs
+++ Assets/Scripts/Camera/CameraColor.cs
@@ -6,12 +6,14 @@ public class CameraColor : MonoBehaviour {
 
 	public float sphereRadius = 10.0f;
 	public float raySize = 1000.0f;
+	public Color myCam;
+	public Color myRay;
 
 	void OnDrawGizmos() 
 	{
-		Gizmos.color = Color.red;
+		Gizmos.color = myCam;
 		Gizmos.DrawSphere(transform.position,  sphereRadius);
-		Gizmos.color = Color.red;
+		Gizmos.color = myRay;
 		Vector3 direction = transform.TransformDirection(Vector3.forward) * raySize;
 		Gizmos.DrawRay(transform.position, direction);
 	}
