9e95cfc36e0535a83936c96ff7c9f5e749c9494f
Home\Алексей <myepam2017rts@gmail.com>
2017-08-27 14:38:22 +0300

    Меню- интерфейс

---
+++ Assets/Scripts/Menu/MenuChecker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuChecker : MonoBehaviour {
+
+	// Use this for initialization
+
+	bool menuState = true;
+
+	public GameObject hud;
+	public GameObject menuInterface;
+
+	void Awake () {
+		Time.timeScale = 1.0f;
+	}
+
+	public void MenuStateChange()
+	{
+		menuState = !menuState;
+		if (menuState) {
+			hud.SetActive (true);
+			menuInterface.SetActive (false);
+			Time.timeScale = 1.0f;
+		} else {
+			hud.SetActive (false);
+			menuInterface.SetActive (true);
+			Time.timeScale = 0.0f;
+		}
+	}
+}
---
+++ Assets/Scripts/Menu/MenuInterface.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuInterface : MonoBehaviour {
+
+	public void NewGame()
+	{
+		Application.LoadLevel("First");
+	}
+
+	public void Exit()
+	{
+		Application.Quit();
+	}
+}
