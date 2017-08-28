234e9c3e6956942ab90fab34e1b585b976fc53cc
Unclemortuary <artem_mihalych@mail.ru>
2017-08-29 01:15:28 +0300

    HUD (almost done)

--- Assets/Scripts/Managers/HUD/ObjectInfo Panel/InfoText.cs
+++ Assets/Scripts/Managers/HUD/ObjectInfo Panel/InfoText.cs
@@ -16,6 +16,10 @@ public class InfoText : MonoBehaviour {
 	[SerializeField]
 	private Text defenseValueText;
 
+	private AbstractGameUnit selectedUnit = null;
+
+	private bool isUnit = false;
+
 	private List<Text> allText = new List<Text>();
 
 	void Start()
@@ -29,19 +33,38 @@ public class InfoText : MonoBehaviour {
 		ClearInfoText ();
 	}
 
-	public void UpdateInfoPanel(AbstractGameUnit unit)
+	void Update()
 	{
-		nameText.text = unit.Description;
-		if (unit.Avatar.GetComponent<BuildingComponent> ())
+		if (isUnit) 
+		{
+			//attackValueText.text = selectedUnit.Characteristics.Attack.ToString()
+			defenseValueText.text = selectedUnit.Characteristics.Defence.ToString();
+		}
+	}
+
+	public void UpdateInfoPanel(AbstractGameUnit unit, bool isBuilding)
+	{
+		selectedUnit = unit;
+		nameText.text = selectedUnit.Description;
+		if (isBuilding)
 			return;
 		else
 		{
-			
+			isUnit = true;
+			SetUnitText ();
 		}
+	}
+
+	private void SetUnitText()
+	{
+		attackText.text = "Attack";
+		defenseText.text = "Defence";
 	}
 
 	public void ClearInfoText()
 	{
+		selectedUnit = null;
+		isUnit = false;
 		for (int i = 0; i < allText.Count; i++)
 			allText [i].text = "";
 	}
