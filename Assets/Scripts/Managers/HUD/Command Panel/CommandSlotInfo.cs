6d0feafc07c0835a7237a211213e2ab71493920a
Home\Алексей <myepam2017rts@gmail.com>
2017-09-03 20:49:24 +0300

    Табличка всплывает

---
+++ Assets/Scripts/Managers/HUD/Command Panel/CommandSlotInfo.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandSlotInfo : MonoBehaviour {
+
+	public ActionData myActionData;
+	public GameObject slotMove;
+	void Awake()
+	{
+		slotMove.SetActive (false);
+	}
+
+	public void SlotInfoEnter()
+	{
+		var slot = GetComponent<CommandSlot> ();
+		//myActionData.ActionsData [slot.Command].PriceToUse [0];
+		for(int i = 0; i < myActionData.ActionsData.Count ;i++)
+		{
+			if (myActionData.ActionsData [i].Action == slot.Command && (myActionData.ActionsData [i].Action.ToString().Contains("reate") 
+				|| myActionData.ActionsData [i].Action.ToString().Contains("Build")))
+			{		
+				RectTransform slotTrans = slotMove.GetComponent<RectTransform> (); 
+				slotTrans.position = new Vector3(Input.mousePosition.x - slotTrans.rect.width/2,Input.mousePosition.y + slotTrans.rect.height/2,slotTrans.position.z );
+
+				slotMove.gameObject.transform.GetChild(1).gameObject.GetComponent<Text>().text = 
+					" " + myActionData.ActionsData [i].PriceToUse.GetResourceAmount((GameResources.ResourceType.STONE));
+				slotMove.gameObject.transform.GetChild(3).gameObject.GetComponent<Text>().text = 
+					" " + myActionData.ActionsData [i].PriceToUse.GetResourceAmount((GameResources.ResourceType.WOOD));
+			
+				slotMove.gameObject.transform.GetChild(5).gameObject.GetComponent<Text>().text = 
+					" " + myActionData.ActionsData [i].PriceToUse.GetResourceAmount((GameResources.ResourceType.FOOD));
+				slotMove.gameObject.transform.GetChild(7).gameObject.GetComponent<Text>().text = 
+					" " + myActionData.ActionsData [i].PriceToUse.GetResourceAmount((GameResources.ResourceType.GOLD));
+				slotMove.SetActive (true);
+				break;
+			}
+		}
+	}
+
+	public void SlotInfoExit()
+	{
+		slotMove.SetActive (false);
+	}
+}
---
+++ Assets/Scripts/Managers/HUD/Command Panel/CommandSlotMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandSlotMove : MonoBehaviour {
+
+
+	RectTransform slotTrans;
+	void Awake () {
+		slotTrans = gameObject.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		slotTrans.position = new Vector3(Input.mousePosition.x - slotTrans.rect.width/2,Input.mousePosition.y + slotTrans.rect.height/2,slotTrans.position.z );
+	}
+}
