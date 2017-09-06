9d6b7023705d0c681f43872aea00409369de5e8a
Home\Алексей <myepam2017rts@gmail.com>
2017-09-06 13:31:37 +0300

    Конец игры

---
+++ Assets/Scripts/Menu/MenuEndGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuEndGame : MonoBehaviour {
+
+	public GameObject endCanvas;
+	public Text endText;
+
+	public void EndMenuActive()
+	{
+		endCanvas.SetActive (true);
+	}
+
+	public void TextString(string text)
+	{
+		endText.text = "" + text;
+		if (endText.text.Contains ("Vict")) {
+			endText.color = Color.green;	
+		} else {
+			endText.color = Color.red;
+		}
+			
+	}
+
+	public void Exit()
+	{
+		//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+		Exit();
+	}
+}
